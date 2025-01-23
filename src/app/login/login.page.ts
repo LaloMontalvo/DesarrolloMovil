fface2a3818f0a0b0d422f3940d02467563588dd
LaloMO <20233l001154@utcv.edu.mx>
2025-01-23 08:12:12 -0600

    Añadir enlaces login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  email : string = 'lalo@gmail.com';
+  password: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  login(){
+    //si usuario = admin y password = admin
+    //entonces console.log('Login correcto')
+    //si no console.log('login incorrecro')
+
+    if(this.email === 'admin' 
+      && this.password ==='admin'){
+      console.log('Login correcto');
+    }
+    else{
+      console.log('Login incorrecto');
+    }
+  }
+
+
+    
 
 }
